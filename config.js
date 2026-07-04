118f32791f45b4bb93aaa31c96ea5ac5629676ae
pro-716 <2g5dr7gcwj@privaterelay.appleid.com>
2026-07-04 04:19:35 +0300

    Add Firebase and bot configuration to config.js

---
+++ config.js
@@ -0,0 +1,12 @@
+// config.js
+export const firebaseConfig = {
+  apiKey: "ضع_هنا_API_KEY",
+  authDomain: "ضع_هنا_AUTH_DOMAIN",
+  projectId: "ضع_هنا_PROJECT_ID",
+  storageBucket: "ضع_هنا_STORAGE_BUCKET",
+  messagingSenderId: "ضع_هنا_SENDER_ID",
+  appId: "ضع_هنا_APP_ID"
+};
+
+export const BOT_TOKEN = "8804335988:AAF3JE7YLRqcjJeFCGQLFoZ1f_pyWrIXDfs";
+export const CHAT_ID = "5044044978";
